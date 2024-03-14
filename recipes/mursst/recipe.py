45b839bcb79aabe62a0afb473f247ab331865e27
Aimee Barciauskas <aimee@developmentseed.org>
2024-03-14 20:51:33 +0000

    Add another option for auth mode

--- recipes/mursst/recipe.py
+++ recipes/mursst/recipe.py
@@ -19,7 +19,7 @@ ED_PASSWORD = os.environ.get('EARTHDATA_PASSWORD')
 auth_mode = os.environ.get('AUTH_MODE', 'edl')
 aws_role_arn = os.environ.get('AWS_ROLE_ARN')
 
-if auth_mode not in ('edl', 'iamrole'):
+if auth_mode not in ('edl', 'iamrole', 'assumerole'):
     raise ValueError(f'Unsupported auth mode: {auth_mode}')
 
 CREDENTIALS_API = 'https://archive.podaac.earthdata.nasa.gov/s3credentials'
@@ -53,27 +53,21 @@ class GetS3Creds(beam.DoFn):
             yield element
             
         if auth_mode == 'iamrole':
-            # If AWS_ROLE_ARN is none, it should be that the current process
-            # is already assuming or using a role that has access to the data,
-            # as in the case of a NASA JupyterHub like hub.openveda.cloud.
-            #
-            # If AWS_ROLE_ARN is NOT none, it should be that the current process
-            # can assume the role and the role has access to the data.
-            if aws_role_arn == None:
-                yield element
-            else:             
-                import boto3
-                client = boto3.client('sts')
-                creds = client.assume_role(
-                    RoleArn=aws_role_arn,
-                    RoleSessionName='mursst-pangeo-forge',
-                    DurationSeconds=43200,
-                )['Credentials']
-                os.environ['AWS_ACCESS_KEY_ID'] = creds['AccessKeyId']
-                os.environ['AWS_SECRET_ACCESS_KEY'] = creds['SecretAccessKey']
-                os.environ['AWS_SESSION_TOKEN'] = creds['SessionToken']
-                print(f"Credentials set via assumed IAM Role {aws_role_arn}")
-                yield element
+            # When role is 'iamrole' the current AWS_ROLE_ARN is appropriate to use.
+            # Useful for using the VEDA JupyterHub.
+            yield element
+        elif auth_mode == 'assume_role':
+            import boto3
+            client = boto3.client('sts')
+            creds = client.assume_role(
+                RoleArn=aws_role_arn,
+                RoleSessionName='mursst-pangeo-forge',
+                DurationSeconds=43200,
+            )['Credentials']
+            os.environ['AWS_ACCESS_KEY_ID'] = creds['AccessKeyId']
+            os.environ['AWS_SECRET_ACCESS_KEY'] = creds['SecretAccessKey']
+            os.environ['AWS_SESSION_TOKEN'] = creds['SessionToken']
+            yield element
         elif auth_mode == 'edl': 
             login_resp = requests.get(CREDENTIALS_API, allow_redirects=False)
             login_resp.raise_for_status()
@@ -95,7 +89,6 @@ class GetS3Creds(beam.DoFn):
             os.environ['AWS_ACCESS_KEY_ID'] = creds['accessKeyId']
             os.environ['AWS_SECRET_ACCESS_KEY'] = creds['secretAccessKey']
             os.environ['AWS_SESSION_TOKEN'] = creds['sessionToken']
-            print("Credentials set via Earthdata Login")
             yield element
     
 recipe = (
